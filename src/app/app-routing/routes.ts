c9f1b10f76cea6b2a1410ea2e446fb0581bf0b65
Isyani <indevinit@gmail.com>
2020-04-02 12:42:51 +0200

    Dialogs

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,9 +8,10 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'aboutus', component: AboutComponent },
     { path: 'menu', component: MenuComponent },
     { path: 'contactus', component: ContactComponent },
-    { path: 'aboutus', component: AboutComponent },
+
     { path: 'dishdetail/:id', component: DishDetailComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatListModule, MatGridListModule, MatButtonModule, MatCardModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
-
-import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishDetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { MatListModule } from '@angular/material/list';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { DishDetailComponent } from './dishdetail/dishdetail.component';
-
-//importing service
 import { DishService } from './services/dish.service';
-
 import { PromotionService } from './services/promotion.service';
-
+import { LeaderService } from './services/leader.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
+
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -36,23 +30,30 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
-    MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    MatCardModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    MatDialogModule,
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
+  entryComponents: [
+    LoginComponent
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
